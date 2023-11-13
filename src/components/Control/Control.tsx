55731e272f85cfc7f234230c0d23fc97fca17b4f
frthid <zvezdochets02@gmail.com>
2023-11-13 10:24:29 +0300

    провел рефакторинг

--- src/components/Control/Control.tsx
+++ src/components/Control/Control.tsx
@@ -8,19 +8,20 @@ import Toggle from '../UI/Toggle/Toggle';
 import { AddTodoArgs } from '../../models/models';
 
 interface ITodoControl {
-  addTodo: ({
-    importance,
-    equipment,
-    message,
-    responsible,
-  }: AddTodoArgs) => void;
+  addTodo: ({ importance, equipment, message, responsible }: AddTodoArgs) => void;
   searchMessage: string;
   setToggle: Dispatch<SetStateAction<string>>;
   setSearchMessage: Dispatch<SetStateAction<string>>;
   toggle: string;
 }
 
-const Control: React.FC<ITodoControl> = ({ addTodo, searchMessage, setSearchMessage, setToggle, toggle}) => {
+const Control: React.FC<ITodoControl> = ({
+  addTodo,
+  searchMessage,
+  setSearchMessage,
+  setToggle,
+  toggle,
+}) => {
   const [popupOpen, setPopupOpen] = useState<boolean>(false);
 
   const handlePopupClick = () => {
@@ -29,16 +30,20 @@ const Control: React.FC<ITodoControl> = ({ addTodo, searchMessage, setSearchMess
 
   const handleToggleClick = () => {
     setToggle((prevToggle: string) => (prevToggle === 'card' ? 'table' : 'card'));
-  }
+  };
 
   const handleInputChange = (value: string) => {
-    setSearchMessage(value)
-  }
+    setSearchMessage(value);
+  };
 
   return (
     <>
       <div className={classes.control}>
-        <Toggle type='button' text={toggle === 'card' ? 'Таблица' : 'Карточки'} onClick={handleToggleClick} />
+        <Toggle
+          type='button'
+          text={toggle === 'card' ? 'Таблица' : 'Карточки'}
+          onClick={handleToggleClick}
+        />
         <Input
           type='text'
           id='search'
@@ -50,6 +55,7 @@ const Control: React.FC<ITodoControl> = ({ addTodo, searchMessage, setSearchMess
         <Popup popupOpen={popupOpen} setPopupOpen={setPopupOpen}>
           <TodoCreator addTodo={addTodo} />
         </Popup>
+
         <Button type='button' text='Создать событие' onClick={handlePopupClick} />
       </div>
     </>
